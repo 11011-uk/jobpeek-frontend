67e0649b9287a7d0e2fce89dc2eabb318d0ff52b
NoIDsea <m.salar11078@gmail.com>
2025-08-20 22:32:33 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,19 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
   },
-  // Optimize for Netlify deployment
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-}
-// next.config.js
-const path = require("path");
-
-module.exports = {
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname);
     return config;
   },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
